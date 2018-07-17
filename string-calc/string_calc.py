836530968a708ba77e97d213e9e6dcf5a6fafa7c
Ruslan Forostianov <ruslan@thehyve.nl>
2018-07-17 19:04:00 +0200

    Refactor using suppress.

--- string-calc/string_calc.py
+++ string-calc/string_calc.py
@@ -1,9 +1,10 @@
+from contextlib import suppress
+
 def add(string):
 	splitted_string = string.split(',')
 	outcome = 0
 	for string_number in splitted_string:
-		if not string_number:
-			string_number = '0' 
-		outcome += int(string_number)
+		with suppress(ValueError):
+			outcome += int(string_number)
 	return str(outcome)
 	
