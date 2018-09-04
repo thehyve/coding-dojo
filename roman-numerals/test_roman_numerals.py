30e07df3cb357b4c7ce2b309a93d2827f0879837
Ruslan Forostianov <ruslan@thehyve.nl>
2018-09-04 18:17:38 +0200

    Add roman numerals kata

---
+++ roman-numerals/test_roman_numerals.py
@@ -0,0 +1,9 @@
+import unittest
+from roman_numerals import to_roman_num
+
+class RomanNumeralsTest(unittest.TestCase):
+	def test_smth(self):
+		self.assertEqual(1, 2)
+
+if __name__ == '__main__':
+    unittest.main() # pragma: no cover
