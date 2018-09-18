8050d9782bad5be1e622d293646d0d6f65a335c2
Ruslan Forostianov <ruslan@thehyve.nl>
2018-09-18 19:19:56 +0200

    implement more requirements for anagrams

--- anagram/anagrams.py
+++ anagram/anagrams.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+from itertools import permutations
+
 def anagrams(word):
     dict_path = Path('/usr/share/dict/words')
+    all_permutations = {''.join(tuple_) for tuple_ in permutations(word)}
     with dict_path.open() as dict_file:
-        if word not in (line.rstrip() for line in dict_file):
-            raise StopIteration()
-    return iter([word])
+        for line in dict_file:
+            line = line.rstrip()
+            if line in all_permutations:
+                yield line
--- anagram/test_anagram.py
+++ anagram/test_anagram.py
@@ -16,11 +16,25 @@ class AnagramTests(unittest.TestCase):
         with self.assertRaises(StopIteration):
             next(output)
 
-    def test_that_nonword_has_no_anagram(self):
-        word = 'b'
+    def test_that_multi_letter_word_is_its_own_anagram(self):
+        word = 'hello'
         output = anagrams(word)
-        with self.assertRaises(StopIteration):
-            next(output)
+        self.assertIn(word, output)
+
+    def test_that_some_multi_letter_words_have_more_than_one_anagram(self):
+        word = 'stop'
+        output = anagrams(word)
+        self.assertTrue(len(list(output)) > 1)
+
+    def test_that_nonword_is_not_a_valid_anagram_of_itself(self):
+        word = 'hjko'
+        output = anagrams(word)
+        self.assertNotIn(word, output)
+
+    def test_that_some_word_has_two_words_anagram(self):
+        word = 'output'
+        output = anagrams(word)
+        self.assertIn(True, ((' ' in result) for result in output), 'jopa')
 
 if __name__ == '__main__':
     unittest.main()
