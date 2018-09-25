2448214b9893896c46f9147af6e00eeaf444bd84
Ruslan Forostianov <ruslan@thehyve.nl>
2018-09-25 19:04:21 +0200

    Refactor out reading eng dictionary

--- anagram/anagrams.py
+++ anagram/anagrams.py
@@ -3,14 +3,19 @@ from itertools import permutations
 
 
 def anagrams(word, n_words=1):
-    spaces_to_add = (n_words-1)*' '
-    word += spaces_to_add
-    dict_path = Path('/usr/share/dict/words')
-    all_permutations = {''.join(tuple_) for tuple_ in permutations(word)}
+    yield from get_anagrams_in_language(word, n_words, read_english_words())
 
+def get_anagrams_in_language(word, n_words, language_words):
+    spaces_to_add = (n_words-1)*' '
+    permutation_input = word + spaces_to_add
+    all_permutations = {''.join(tuple_) for tuple_ in permutations(permutation_input)}
+    for potential_anagram in all_permutations:
+        potential_anagram_words = potential_anagram.split(' ')
+        if all(word in language_words for word in potential_anagram_words):
+            yield potential_anagram
+
+def read_english_words():
+    dict_path = Path('/usr/share/dict/words')
     with dict_path.open() as dict_file:
         dict_set = {line.rstrip() for line in dict_file}
-        for potential_anagram in all_permutations:
-            potential_anagram_words = potential_anagram.split(' ')
-            if all(word in dict_set for word in potential_anagram_words):
-                yield potential_anagram
+    return dict_set
