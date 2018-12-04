60572b106238fd02fe9baed2353cc8df4abd26c0
Ruslan Forostianov <ruslan@thehyve.nl>
2018-12-04 19:21:38 +0100

    Make int options work

--- dojo-6-argument-parser/arg_parser.py
+++ dojo-6-argument-parser/arg_parser.py
@@ -5,4 +5,14 @@ def parse_args(schema, args):
             in args
             if arg.startswith('-')):
         raise ValueError
-    return {arg: ('-' + arg in args) for arg in schema}
+    result = {}
+    for arg, arg_type in schema.items():
+        if arg_type == 'flag':
+            result[arg] = ('-' + arg) in args
+        elif arg_type == 'int':
+            arg_index = args.index('-' + arg)
+            if arg_index == -1:
+                result[arg] = 0
+            else:
+                result[arg] = int(args[arg_index+1])
+    return result
