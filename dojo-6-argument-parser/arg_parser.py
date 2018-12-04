8ea7b10d5561d4919d2470ba850f2bc3e330a8cf
Ruslan Forostianov <ruslan@thehyve.nl>
2018-12-04 19:19:09 +0100

    Fix Exception when providing non-opt arg

--- dojo-6-argument-parser/arg_parser.py
+++ dojo-6-argument-parser/arg_parser.py
@@ -1,5 +1,8 @@
 def parse_args(schema, args):
     """Parse the arguments according to the schema."""
-    if any(arg.lstrip('-') not in schema for arg in args):
+    if any(arg.lstrip('-') not in schema
+            for arg
+            in args
+            if arg.startswith('-')):
         raise ValueError
     return {arg: ('-' + arg in args) for arg in schema}
