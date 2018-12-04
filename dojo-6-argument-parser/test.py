2d08ff88b354a8466bb447116fc4bd2546a8f84f
Ruslan Forostianov <ruslan@thehyve.nl>
2018-12-04 18:43:55 +0100

    Rename tests for clarity and correctness

--- dojo-6-argument-parser/test.py
+++ dojo-6-argument-parser/test.py
@@ -4,7 +4,7 @@ import unittest
 from arg_parser import parse_args
 
 class ArgumentParserCase(unittest.TestCase):
-    def test_that_logging_is_false_if_not_passed(self):
+    def test_that_flag_is_false_if_not_passed(self):
         """If there's no -l argument, value should be false."""
         # given
         arg_list = []
@@ -14,7 +14,7 @@ class ArgumentParserCase(unittest.TestCase):
         # then
         self.assertFalse(parsed_args['l'])
 
-    def test_that_logging_is_true_if_passed(self):
+    def test_that_flag_is_true_if_passed(self):
         """If there is an -l argument, value should be true."""
         # given
         arg_list = ['-l']
@@ -24,7 +24,7 @@ class ArgumentParserCase(unittest.TestCase):
         # then
         self.assertTrue(parsed_args['l'])
 
-    def test_that_false_returned_when_arg_not_provided_but_args_not_empty(self):
+    def test_that_flag_is_false_when_other_flag_passed(self):
         # given
         arg_list = ['-l']
         schema = {'p':'flag', 'l':'flag'}
