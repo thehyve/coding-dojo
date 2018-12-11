92fd1f394eb20d2086ad94fd57af3f77b9ddf206
Ruslan Forostianov <ruslan@thehyve.nl>
2018-12-11 18:54:36 +0100

    Extract invalid arg identification

--- dojo-6-argument-parser/arg_parser.py
+++ dojo-6-argument-parser/arg_parser.py
@@ -1,10 +1,13 @@
-def parse_args(schema, args):
-    """Parse the arguments according to the schema."""
-    if any(arg.lstrip('-') not in schema
+def _has_unknown_opt(args, schema):
+    return any(arg.lstrip('-') not in schema
             for arg
             in args
             if arg.startswith('-') and not arg[1:].isdigit()
-            ):
+            )
+
+def parse_args(schema, args):
+    """Parse the arguments according to the schema."""
+    if _has_unknown_opt(args, schema):
         raise ValueError()
     result = {}
     for arg, arg_type in schema.items():
