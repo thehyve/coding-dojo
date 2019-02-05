f4167b7ff9d91831ddae195b59080cd519b07983
Ruslan Forostianov <ruslan@thehyve.nl>
2019-02-05 19:04:28 +0100

    Implement dead neighbours

--- life/life.py
+++ life/life.py
@@ -1,4 +1,4 @@
 """Game of life; see test for spec."""
 
 def live(grid):
-    return ['.']
+    return ['.'] if len(grid[0]) == 1 else ['..']
--- life/test_life.py
+++ life/test_life.py
@@ -16,6 +16,21 @@ class LifeTestCase(unittest.TestCase):
         new_gen = live(grid)
         self.assertEqual(new_gen, ['.'])
 
+    def test_that_two_dead_cells_remain_dead(self):
+        grid = ['..']
+        new_gen = live(grid)
+        self.assertEqual(new_gen, ['..'])
+
+    def test_that_two_live_cells_die(self):
+        grid = ['**']
+        new_gen = live(grid)
+        self.assertEqual(new_gen, ['..'])
+
+    def test_that_live_cell_next_to_dead_cell_dies(self):
+        grid = ['*.']
+        new_gen = live(grid)
+        self.assertEqual(new_gen, ['..'])
+
 
 if __name__ == '__main__':
     unittest.main()
