f9ce938052b85f5d46b92f8fca0c70064adbeec3
Ruslan Forostianov <ruslan@thehyve.nl>
2019-02-05 19:15:49 +0100

    Factor out dups

--- life/test_life.py
+++ life/test_life.py
@@ -6,30 +6,24 @@ from life import live
 
 class LifeTestCase(unittest.TestCase):
 
+    def assertNextGen(self, gen1, gen2):
+        new_gen = live(gen1)
+        self.assertEqual(new_gen, gen2)
+
     def test_that_cell_without_neighbours_remains_dead(self):
-        grid = ['.']
-        new_gen = live(grid)
-        self.assertEqual(new_gen, ['.'])
+        self.assertNextGen(['.'], ['.'])
 
     def test_that_living_cell_without_neighbours_dies(self):
-        grid = ['*']
-        new_gen = live(grid)
-        self.assertEqual(new_gen, ['.'])
+        self.assertNextGen(['*'], ['.'])
 
     def test_that_two_dead_cells_remain_dead(self):
-        grid = ['..']
-        new_gen = live(grid)
-        self.assertEqual(new_gen, ['..'])
+        self.assertNextGen(['..'], ['..'])
 
     def test_that_two_live_cells_die(self):
-        grid = ['**']
-        new_gen = live(grid)
-        self.assertEqual(new_gen, ['..'])
+        self.assertNextGen(['**'], ['..'])
 
     def test_that_live_cell_next_to_dead_cell_dies(self):
-        grid = ['*.']
-        new_gen = live(grid)
-        self.assertEqual(new_gen, ['..'])
+        self.assertNextGen(['*.'], ['..'])
 
 
 if __name__ == '__main__':
