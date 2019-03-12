18666c1ebc362016443173f57469ccb6410ca680
Ruslan Forostianov <ruslan@thehyve.nl>
2019-03-12 19:05:09 +0100

    Add encryption

--- code-cracker/code_cracker.py
+++ code-cracker/code_cracker.py
@@ -1,32 +1,37 @@
-def decrypt(msg):
-    decryption_key = {
-        '!': 'a',
-        ')': 'b',
-        '"': 'c',
-        '(': 'd',
-        '£': 'e',
-        '*': 'f',
-        '%': 'g',
-        '&': 'h',
-        '>': 'i',
-        '<': 'j',
-        '@': 'k',
-        'a': 'l',
-        'b': 'm',
-        'c': 'n',
-        'd': 'o',
-        'e': 'p',
-        'f': 'q',
-        'g': 'r',
-        'h': 's',
-        'i': 't',
-        'j': 'u',
-        'k': 'v',
-        'l': 'w',
-        'm': 'x',
-        'n': 'y',
-        'o': 'z',
-        ' ': ' ',
-    }
+decryption_key = {
+    '!': 'a',
+    ')': 'b',
+    '"': 'c',
+    '(': 'd',
+    '£': 'e',
+    '*': 'f',
+    '%': 'g',
+    '&': 'h',
+    '>': 'i',
+    '<': 'j',
+    '@': 'k',
+    'a': 'l',
+    'b': 'm',
+    'c': 'n',
+    'd': 'o',
+    'e': 'p',
+    'f': 'q',
+    'g': 'r',
+    'h': 's',
+    'i': 't',
+    'j': 'u',
+    'k': 'v',
+    'l': 'w',
+    'm': 'x',
+    'n': 'y',
+    'o': 'z',
+    ' ': ' ',
+}
 
+def decrypt(msg):
     return ''.join(map(lambda s: decryption_key[s], msg))
+
+encryption_key = {v: k for k, v in decryption_key.items()}
+
+def encrypt(msg):
+    return ''.join(map(lambda s: encryption_key[s], msg))
--- code-cracker/test_code_cracker.py
+++ code-cracker/test_code_cracker.py
@@ -1,5 +1,5 @@
 import unittest
-from code_cracker import decrypt
+from code_cracker import decrypt, encrypt
 
 class CodeCrackerTestCase(unittest.TestCase):
     
@@ -10,6 +10,10 @@ class CodeCrackerTestCase(unittest.TestCase):
     def test_reverse_alphabet_translation(self):
         decrypted_msg = decrypt('! ) " ( £ * % & > < @ a b c d e f g h i j k l m n o'[::-1])
         self.assertEqual(decrypted_msg, 'a b c d e f g h i j k l m n o p q r s t u v w x y z'[::-1])
+
+    def test_alphabet_retranslation(self):
+        encrypted_msg = encrypt('a b c d e f g h i j k l m n o p q r s t u v w x y z')
+        self.assertEqual(encrypted_msg, '! ) " ( £ * % & > < @ a b c d e f g h i j k l m n o')
 
 if __name__ == '__main__':
     unittest.main()
