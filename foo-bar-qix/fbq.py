f7ad21d0d64952a69c02a1b8660f11c3a47519a9
Ruslan Forostianov <ruslan@thehyve.nl>
2019-05-14 19:12:47 +0200

    Replace if-s with dictionary

--- foo-bar-qix/fbq.py
+++ foo-bar-qix/fbq.py
@@ -1,10 +1,13 @@
 def compute(arg):
-    if arg == '3':
-        return 'FooFoo'
-    if arg == '5':
-        return 'BarBar'
-    if arg == '7':
-        return 'QixQix'
+    special_cases = {
+        '3': 'FooFoo',
+        '5': 'BarBar',
+        '7': 'QixQix',
+    }
+    try:
+        return special_cases[arg]
+    except KeyError:
+        pass
     if int(arg) % 3 == 0:
         return 'Foo'
     return arg
