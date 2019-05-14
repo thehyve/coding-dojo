30df98922ee225f653cf7fe803b534ee8327a4f4
Ruslan Forostianov <ruslan@thehyve.nl>
2019-05-14 19:16:57 +0200

    abstract suppressing exception

--- foo-bar-qix/fbq.py
+++ foo-bar-qix/fbq.py
@@ -1,13 +1,14 @@
+from contextlib import suppress
+
+
 def compute(arg):
     special_cases = {
         '3': 'FooFoo',
         '5': 'BarBar',
         '7': 'QixQix',
     }
-    try:
+    with suppress(KeyError):
         return special_cases[arg]
-    except KeyError:
-        pass
     if int(arg) % 3 == 0:
         return 'Foo'
     return arg
