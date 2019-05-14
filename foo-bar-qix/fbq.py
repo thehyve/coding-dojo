0b0fdbfa117e62961cb87d7f17b1e1e88a4dc9aa
Ruslan Forostianov <ruslan@thehyve.nl>
2019-05-14 18:49:23 +0200

    Return Foo when divisible by 3

--- foo-bar-qix/fbq.py
+++ foo-bar-qix/fbq.py
@@ -1,2 +1,4 @@
 def compute(arg):
+    if int(arg) % 3 == 0:
+        return 'Foo'
     return arg
--- foo-bar-qix/test.py
+++ foo-bar-qix/test.py
@@ -15,5 +15,11 @@ class FooBarQixTestCase(unittest.TestCase):
     def test_that_passing_two_results_in_two(self):
         self.assertResultsIn('2', '2')
 
+    def test_that_passing_six_results_in_Foo(self):
+        self.assertResultsIn('6', 'Foo')
+
+    def test_that_passing_nine_results_in_Foo(self):
+        self.assertResultsIn('9', 'Foo')
+
 if __name__ == '__main__':
     unittest.main()
