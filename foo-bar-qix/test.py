b05f8d37217e33b86429b2689bc068bdb26075c1
Ruslan Forostianov <ruslan@thehyve.nl>
2019-05-14 18:43:14 +0200

    Dedupe test code

--- foo-bar-qix/test.py
+++ foo-bar-qix/test.py
@@ -5,13 +5,15 @@ from fbq import compute
 
 class FooBarQixTestCase(unittest.TestCase):
 
+    def assertResultsIn(self, arg, expected):
+        result = compute(arg)
+        self.assertEqual(result, expected)
+
     def test_that_passing_one_results_in_one(self):
-        result = compute('1')
-        self.assertEqual(result, '1')
+        self.assertResultsIn('1', '1')
 
     def test_that_passing_two_results_in_two(self):
-        result = compute('2')
-        self.assertEqual(result, '2')
+        self.assertResultsIn('2', '2')
 
 if __name__ == '__main__':
     unittest.main()
