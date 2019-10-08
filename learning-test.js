aabedfc188d565ca0b7ed9a31a75cc42edfe62d1
The Hyve Coding Dojo <noreply@thehyve.nl>
2019-10-08 19:24:00 +0200

    Assert that Viewer adds an element

--- learning-test.js
+++ learning-test.js
@@ -1,9 +1,21 @@
 /* eslint-env mocha */
 import pv from 'bio-pv'
+import assert from 'assert'
+
+describe('a newly created element', function () {
+  it('has no children', function () {
+    var container = document.createElement('div')
+    document.body.appendChild(container)
+
+    var numberOfChildren = container.childElementCount
+
+    assert.strictEqual(numberOfChildren, 0)
+  })
+})
 
 describe('pv', function () {
   describe('#Viewer()', function () {
-    it('should add an element to the DOM', function () {
+    it('adds an element to the DOM', function () {
       var options = {
         width: 600,
         height: 600,
@@ -12,7 +24,10 @@ describe('pv', function () {
       }
       var container = document.createElement('div')
       document.body.appendChild(container)
+
       pv.Viewer(container, options)
+
+      assert.strictEqual(container.childElementCount, 1)
     })
   })
 })
